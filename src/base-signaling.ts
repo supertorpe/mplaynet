37eeff98cfb2f53ef7646b4cc133600256d37f77
supertorpe <supertorpe@hotmail.com>
2021-01-16 23:26:30 +0100

    Separate the signaling logic to a base class to make it independent of the storage used

---
+++ src/base-signaling.ts
@@ -0,0 +1,131 @@
+import { EventEmitter } from "./event-emitter";
+import { Mesh } from "./mesh";
+import { PairingItem, PairingRecord, PeerRecord, RoomRecord } from "./records";
+
+export abstract class BaseSignaling {
+
+    protected _uuid: string = '';
+    protected roomRecord: RoomRecord | null = null;
+    protected _roomRecordEmitter: EventEmitter<RoomRecord>;
+    protected pairingMap: Map<string, PairingItem> = new Map();
+
+    get roomRecordEmitter(): EventEmitter<RoomRecord> {
+        return this._roomRecordEmitter;
+    }
+
+    constructor() {
+        this._roomRecordEmitter = new EventEmitter<RoomRecord>();
+    }
+
+    public abstract hostRoom(roomId: string, username: string, uuid: string): void;
+    public abstract joinRoom(roomId: string, username: string, uuid: string): Promise<boolean>;
+    protected abstract saveRoomInfo(): void;
+    protected abstract saveIceCandidate(uuid: string, candidate: string): void;
+    protected abstract savePairing(mesh: Mesh, myIndex: number, index: number, peer: PeerRecord): void;
+
+    public upatePlayerStatus(ready: boolean) {
+        if (this.roomRecord) {
+            const peer = this.roomRecord.peers.find(
+                (peer) => peer.uuid === this._uuid
+            );
+            if (peer) {
+                peer.ready = ready;
+                this.saveRoomInfo();
+            }
+        }
+    }
+
+    public startPairings(mesh: Mesh) {
+        return new Promise<boolean>((resolve) => {
+            mesh.channelOpenEmitter.addEventListener((_uuid, _event) => {
+                console.log('mesh opened');
+                resolve(true);
+            });
+            mesh.iceCandidateEmitter.addEventListener((uuid, candidate) => {
+                let sCandidate = JSON.stringify(candidate);
+                if (!candidate.candidate) return;
+                console.log('candidate: ' + sCandidate);
+                this.saveIceCandidate(uuid, sCandidate);
+            });
+            if (this.roomRecord) {
+                const myIndex = this.roomRecord.peers.findIndex((peer) => peer.uuid === this._uuid);
+                for (let [index, peer] of this.roomRecord.peers.entries()) {
+                    if (index === myIndex) {
+                        continue;
+                    }
+                    this.savePairing(mesh, myIndex, index, peer);
+                }
+            }
+        });
+    }
+
+    protected abstract savePairingRecord(info: PairingRecord, uuid: string): void;
+
+    protected pairingRecordChanged(mesh: Mesh, info: PairingRecord, peer: PeerRecord, myIndex: number, index: number) {
+        if (!info || !info.status) {
+            if (myIndex < index) {
+                mesh.createConnection(peer.uuid);
+                mesh.createOffer(peer.uuid).then((offer) => {
+                    console.log('create offer');
+                    info = new PairingRecord(
+                        1,
+                        this._uuid,
+                        peer.uuid,
+                        [],
+                        [],
+                        JSON.stringify(offer),
+                        ''
+                    );
+                    this.pairingMap.set(
+                        peer.uuid,
+                        new PairingItem([], false, false)
+                    );
+                    this.savePairingRecord(info, peer.uuid);
+                });
+            }
+        } else {
+            let pairingItem = this.pairingMap.get(peer.uuid);
+            if (!pairingItem) {
+                pairingItem = new PairingItem([], false, false);
+                this.pairingMap.set(peer.uuid, pairingItem);
+            }
+            if (myIndex > index && info.offer && !pairingItem.answered) {
+                pairingItem.answered = true;
+                mesh.createConnection(peer.uuid);
+                mesh
+                    .createAnswer(peer.uuid, JSON.parse(info.offer))
+                    .then((answer) => {
+                        console.log('create answer');
+                        info.answer = JSON.stringify(answer);
+                        this.savePairingRecord(info, peer.uuid);
+                    });
+            }
+            if (
+                myIndex < index &&
+                info.answer &&
+                !pairingItem.answerVerified
+            ) {
+                console.log('verify answer');
+                pairingItem.answerVerified = true;
+                mesh.verifyAnswer(peer.uuid, JSON.parse(info.answer));
+            }
+            let candidates: string[];
+            if (myIndex < index) {
+                candidates = info.iceCandidates2;
+            } else {
+                candidates = info.iceCandidates1;
+            }
+            candidates.forEach((candidate) => {
+                if (
+                    pairingItem &&
+                    !pairingItem.iceCandidatesDone.includes(candidate)
+                ) {
+                    pairingItem.iceCandidatesDone.push(candidate);
+                    console.log('add candidate: ' + JSON.stringify(candidate));
+                    mesh.addIceCandidate(peer.uuid, JSON.parse(candidate));
+                }
+            });
+        }
+    }
+
+}
--- src/deepstream-signaling.ts
+++ src/deepstream-signaling.ts
@@ -1,27 +1,24 @@
 import { DeepstreamClient } from '@deepstream/client';
 import { Record } from '@deepstream/client/dist/src/record/record';
-import { EventEmitter } from './event-emitter';
+import { BaseSignaling } from './base-signaling';
 import { Mesh } from './mesh';
-import { RoomRecord, PeerRecord, PairingRecord, PairingItem } from './records';
+import { RoomRecord, PeerRecord, PairingRecord } from './records';
 
-export class DeepstreamSignaling {
+export class DeepstreamSignaling extends BaseSignaling {
+
   private deepstreamClient: DeepstreamClient;
   private room: Record | null = null;
   private subscriptions: Record[] = [];
-  private roomRecord: RoomRecord | null = null;
-  private _uuid: string = '';
   private pairRecordMap: Map<string, Record> = new Map();
-  private pairingMap: Map<string, PairingItem> = new Map();
-  private _roomRecordEmitter: EventEmitter<RoomRecord>;
 
   constructor(deepstreamUrl: string) {
-    this._roomRecordEmitter = new EventEmitter<RoomRecord>();
+    super();
     this.deepstreamClient = new DeepstreamClient(deepstreamUrl);
     this.deepstreamClient.login();
   }
 
-  get roomRecordEmitter(): EventEmitter<RoomRecord> {
-    return this._roomRecordEmitter;
+  public close() {
+    this.subscriptions.forEach((record) => record.unsubscribe(() => { }));
   }
 
   private bindRoom(roomId: string, username: string, uuid: string) {
@@ -36,7 +33,7 @@ export class DeepstreamSignaling {
       }
       if (!this.roomRecord.peers.some((peer) => peer.uuid === uuid)) {
         this.roomRecord.peers.push(new PeerRecord(username, uuid, false));
-        this.saveRoomRecord();
+        this.saveRoomInfo();
       } else {
         this._roomRecordEmitter.notify(uuid, info);
       }
@@ -56,166 +53,72 @@ export class DeepstreamSignaling {
     });
   }
 
-  public close() {
-    this.subscriptions.forEach((record) => record.unsubscribe(() => {}));
-  }
-
-  public upatePlayerStatus(ready: boolean) {
-    if (this.roomRecord) {
-      const peer = this.roomRecord.peers.find(
-        (peer) => peer.uuid === this._uuid
-      );
-      if (peer) {
-        peer.ready = ready;
-        this.saveRoomRecord();
-      }
-    }
-  }
-
-  private saveRoomRecord() {
+  protected saveRoomInfo() {
     if (this.room) this.room.set(JSON.parse(JSON.stringify(this.roomRecord)));
   }
 
-  public startPairings(mesh: Mesh) {
-    return new Promise<boolean>((resolve) => {
-      mesh.channelOpenEmitter.addEventListener((_uuid, _event) => {
-        console.log('mesh opened');
-        resolve(true);
-      });
-      mesh.iceCandidateEmitter.addEventListener((uuid, candidate) => {
-        let sCandidate = JSON.stringify(candidate);
-        if (!candidate.candidate) return;
-        console.log('candidate: ' + sCandidate);
-        const pairRecord = this.pairRecordMap.get(uuid);
-        if (pairRecord) {
-          const pairingRecord: PairingRecord = pairRecord.get();
-          if (uuid === pairingRecord.uuid1) {
-            pairingRecord.iceCandidates2.push(sCandidate);
-          } else {
-            pairingRecord.iceCandidates1.push(sCandidate);
-          }
-          pairRecord.set(JSON.parse(JSON.stringify(pairingRecord)));
-        }
-      });
-      if (this.roomRecord) {
-        const myIndex = this.roomRecord.peers.findIndex(
-          (peer) => peer.uuid === this._uuid
-        );
-        for (let [index, peer] of this.roomRecord.peers.entries()) {
-          if (index === myIndex) {
-            continue;
-          }
-          const pairRecordName = `pair_${this.roomRecord.roomId}_${Math.min(
-            index,
-            myIndex
-          )}_${Math.max(index, myIndex)}`;
-          const pairRecord = this.deepstreamClient.record.getRecord(
-            pairRecordName
-          );
+  protected saveIceCandidate(uuid: string, candidate: string): void {
+    const pairRecord = this.pairRecordMap.get(uuid);
+    if (pairRecord) {
+      const pairingRecord: PairingRecord = pairRecord.get();
+      if (uuid === pairingRecord.uuid1) {
+        pairingRecord.iceCandidates2.push(candidate);
+      } else {
+        pairingRecord.iceCandidates1.push(candidate);
+      }
+      pairRecord.set(JSON.parse(JSON.stringify(pairingRecord)));
+    }
+  }
 
-          pairRecord.setMergeStrategy(
-            (
-              localValue: any,
-              _localVersion,
-              remoteValue: any,
-              _remoteVersion,
-              callback
-            ) => {
-              if (remoteValue.status > localValue.status)
-                localValue.status = remoteValue.status;
-              if (!localValue.uuid1 && remoteValue.uuid1)
-                localValue.uuid1 = remoteValue.uuid1;
-              if (!localValue.uuid2 && remoteValue.uuid2)
-                localValue.uuid2 = remoteValue.uuid2;
-              if (!localValue.answer && remoteValue.answer)
-                localValue.answer = remoteValue.answer;
-              remoteValue.iceCandidates1.forEach((candidate: any) => {
-                if (!localValue.iceCandidates1.includes(candidate)) {
-                  localValue.iceCandidates1.push(candidate);
-                }
-              });
-              remoteValue.iceCandidates2.forEach((candidate: any) => {
-                if (!localValue.iceCandidates2.includes(candidate)) {
-                  localValue.iceCandidates2.push(candidate);
-                }
-              });
-              if (!localValue.offer && remoteValue.offer)
-                localValue.offer = remoteValue.offer;
-              if (!localValue.answer && remoteValue.answer)
-                localValue.answer = remoteValue.answer;
-              callback(null, localValue);
-            }
-          );
-          this.pairRecordMap.set(peer.uuid, pairRecord);
-          this.subscriptions.push(pairRecord);
-          pairRecord.subscribe((info: PairingRecord) => {
-            if (!info || !info.status) {
-              if (myIndex < index) {
-                mesh.createConnection(peer.uuid);
-                mesh.createOffer(peer.uuid).then((offer) => {
-                  console.log('create offer');
-                  info = new PairingRecord(
-                    1,
-                    this._uuid,
-                    peer.uuid,
-                    [],
-                    [],
-                    JSON.stringify(offer),
-                    ''
-                  );
-                  this.pairingMap.set(
-                    peer.uuid,
-                    new PairingItem([], false, false)
-                  );
-                  pairRecord.set(JSON.parse(JSON.stringify(info)));
-                });
-              }
-            } else {
-              let pairingItem = this.pairingMap.get(peer.uuid);
-              if (!pairingItem) {
-                pairingItem = new PairingItem([], false, false);
-                this.pairingMap.set(peer.uuid, pairingItem);
-              }
-              if (myIndex > index && info.offer && !pairingItem.answered) {
-                pairingItem.answered = true;
-                mesh.createConnection(peer.uuid);
-                mesh
-                  .createAnswer(peer.uuid, JSON.parse(info.offer))
-                  .then((answer) => {
-                    console.log('create answer');
-                    info.answer = JSON.stringify(answer);
-                    pairRecord.set(JSON.parse(JSON.stringify(info)));
-                  });
-              }
-              if (
-                myIndex < index &&
-                info.answer &&
-                !pairingItem.answerVerified
-              ) {
-                console.log('verify answer');
-                pairingItem.answerVerified = true;
-                mesh.verifyAnswer(peer.uuid, JSON.parse(info.answer));
-              }
-              let candidates: string[];
-              if (myIndex < index) {
-                candidates = info.iceCandidates2;
-              } else {
-                candidates = info.iceCandidates1;
-              }
-              candidates.forEach((candidate) => {
-                if (
-                  pairingItem &&
-                  !pairingItem.iceCandidatesDone.includes(candidate)
-                ) {
-                  pairingItem.iceCandidatesDone.push(candidate);
-                  console.log('add candidate: ' + JSON.stringify(candidate));
-                  mesh.addIceCandidate(peer.uuid, JSON.parse(candidate));
-                }
-              });
+  protected savePairing(mesh: Mesh, myIndex: number, index: number, peer: PeerRecord): void {
+    if (this.roomRecord) {
+      const pairRecordName = `pair_${this.roomRecord.roomId}_${Math.min(index, myIndex)}_${Math.max(index, myIndex)}`;
+      const pairRecord = this.deepstreamClient.record.getRecord(pairRecordName);
+      pairRecord.setMergeStrategy(
+        (
+          localValue: any,
+          _localVersion: any,
+          remoteValue: any,
+          _remoteVersion: any,
+          callback: (arg0: null, arg1: any) => void
+        ) => {
+          if (remoteValue.status > localValue.status)
+            localValue.status = remoteValue.status;
+          if (!localValue.uuid1 && remoteValue.uuid1)
+            localValue.uuid1 = remoteValue.uuid1;
+          if (!localValue.uuid2 && remoteValue.uuid2)
+            localValue.uuid2 = remoteValue.uuid2;
+          if (!localValue.answer && remoteValue.answer)
+            localValue.answer = remoteValue.answer;
+          remoteValue.iceCandidates1.forEach((candidate: any) => {
+            if (!localValue.iceCandidates1.includes(candidate)) {
+              localValue.iceCandidates1.push(candidate);
             }
           });
+          remoteValue.iceCandidates2.forEach((candidate: any) => {
+            if (!localValue.iceCandidates2.includes(candidate)) {
+              localValue.iceCandidates2.push(candidate);
+            }
+          });
+          if (!localValue.offer && remoteValue.offer)
+            localValue.offer = remoteValue.offer;
+          if (!localValue.answer && remoteValue.answer)
+            localValue.answer = remoteValue.answer;
+          callback(null, localValue);
         }
-      }
-    });
+      );
+      this.pairRecordMap.set(peer.uuid, pairRecord);
+      this.subscriptions.push(pairRecord);
+      pairRecord.subscribe((info: PairingRecord) => {
+        this.pairingRecordChanged(mesh, info, peer, myIndex, index);
+      });
+    }
   }
+
+  protected savePairingRecord(info: PairingRecord, uuid: string): void {
+    const pairRecord = this.pairRecordMap.get(uuid);
+    if (pairRecord)
+      pairRecord.set(JSON.parse(JSON.stringify(info)));
+  }
+
 }
